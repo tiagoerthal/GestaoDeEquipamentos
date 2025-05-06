3b39afb047b77f8bc62637f7956b338d6a610df2
Tiago <tiagoeweber@gmail.com>
2025-05-06 17:10:25 -0300

    Criado a class TelaEquipamentos e Equipamentos para gerar o menu inicial

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -4,6 +4,55 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        TelaEquipamento telaEquipamento = new TelaEquipamento();
+       while (true)
+        {
+            char opcaoEscolhida = telaEquipamento.ApresentarMenu();
+
+            if (opcaoEscolhida == 'S')
+                break;
+
+            switch (opcaoEscolhida)
+            {
+                case '1':
+                    Console.WriteLine();
+                    break;
+            }
+
+        }
+
+
     }
+}
+
+public class TelaEquipamento
+{
+    public char ApresentarMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Gestão De Equipamentos");
+
+        Console.WriteLine();
+
+        Console.WriteLine("1- Cadastro de Equipamento");
+        Console.WriteLine("S- Sair");
+
+        Console.WriteLine();
+
+        Console.Write("Digite uma opção válida: ");
+        char opcaoEscolhida = Console.ReadLine().ToUpper()[0];
+
+        return opcaoEscolhida;
+    }
+}
+
+public class Equipamento
+{
+    public int id;
+    public string nome;
+    public decimal precoAquisicao;
+    public string numeroSerie;
+    public string fabricante;
+    public DateTime dataFabricacao;
+
 }
