bc1ab5600803b5d923eadc62792112fee5ae12c9
Tiago <tiagoeweber@gmail.com>
2025-05-06 17:57:08 -0300

    Capaz de escrever e guardar as informações escritas pelo usuario

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace GestaoDeEquipamentos.ConsoleApp;
 
-internal class Program
+class Program
 {
     static void Main(string[] args)
     {
@@ -15,23 +15,26 @@ internal class Program
             switch (opcaoEscolhida)
             {
                 case '1':
-                    Console.WriteLine();
+                    telaEquipamento.CadastrarRegistro();
                     break;
             }
-
         }
-
-
     }
 }
 
 public class TelaEquipamento
 {
+    public void ExibirCabecalho()
+    {
+        Console.Clear();
+        Console.WriteLine("Gestão De Equipamentos");
+        Console.WriteLine();
+    }
+
     public char ApresentarMenu()
     {
         Console.Clear();
         Console.WriteLine("Gestão De Equipamentos");
-
         Console.WriteLine();
 
         Console.WriteLine("1- Cadastro de Equipamento");
@@ -43,6 +46,41 @@ public class TelaEquipamento
         char opcaoEscolhida = Console.ReadLine().ToUpper()[0];
 
         return opcaoEscolhida;
+    }
+
+    public void CadastrarRegistro()
+    {
+        Console.Clear();
+        Console.WriteLine("Gestão De Equipamentos");
+        Console.WriteLine();
+
+        Console.WriteLine("Cadastro De Equipamentos");
+        Console.WriteLine();
+
+        Console.Write("Digite o nome do equipamento: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite o preço de aquisição do equipamento: ");
+        decimal precoAquisicao = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Digite o número de série do equipamento: ");
+        string numeroSerie = Console.ReadLine();
+
+        Console.Write("Digite o nome do fabricante do equipamento: ");
+        string fabricante = Console.ReadLine();
+
+        Console.Write("Digite a data de fabricação do equipamento: ");
+        DateTime dataFabricacao = DateTime.Parse(Console.ReadLine());
+
+        Equipamento equipamento = new Equipamento();
+        equipamento.nome = nome;
+        equipamento.precoAquisicao = precoAquisicao;
+        equipamento.numeroSerie = numeroSerie;
+        equipamento.fabricante = fabricante;
+        equipamento.dataFabricacao = dataFabricacao;    
+        
+        Console.WriteLine($"\nEquipamento \"{equipamento.nome}\" cadastrado com sucesso!");
+        Console.ReadLine();
     }
 }
 
