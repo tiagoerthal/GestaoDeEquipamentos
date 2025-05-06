f479a6bb2db81874f26c269b7630a19bf6c765f5
Tiago <tiagoeweber@gmail.com>
2025-05-06 18:48:24 -0300

    capaz de visualizar o registro de dados

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -17,13 +17,19 @@ class Program
                 case '1':
                     telaEquipamento.CadastrarRegistro();
                     break;
+
+                case '2':
+                    telaEquipamento.VisualizarRegistros();
+                    break;
             }
         }
     }
 }
-
+ // Apresentação
 public class TelaEquipamento
 {
+    public RepositorioEquipamento repositorioEquipamento = new RepositorioEquipamento();
+
     public void ExibirCabecalho()
     {
         Console.Clear();
@@ -33,11 +39,10 @@ public class TelaEquipamento
 
     public char ApresentarMenu()
     {
-        Console.Clear();
-        Console.WriteLine("Gestão De Equipamentos");
-        Console.WriteLine();
+        ExibirCabecalho();
 
         Console.WriteLine("1- Cadastro de Equipamento");
+        Console.WriteLine("2- Visualizar Equipamento");
         Console.WriteLine("S- Sair");
 
         Console.WriteLine();
@@ -50,26 +55,24 @@ public class TelaEquipamento
 
     public void CadastrarRegistro()
     {
-        Console.Clear();
-        Console.WriteLine("Gestão De Equipamentos");
-        Console.WriteLine();
+        ExibirCabecalho();
 
         Console.WriteLine("Cadastro De Equipamentos");
         Console.WriteLine();
 
-        Console.Write("Digite o nome do equipamento: ");
+        Console.WriteLine("Digite o nome do equipamento: ");
         string nome = Console.ReadLine();
 
-        Console.Write("Digite o preço de aquisição do equipamento: ");
+        Console.WriteLine("Digite o preço de aquisição do equipamento: ");
         decimal precoAquisicao = Convert.ToDecimal(Console.ReadLine());
 
-        Console.Write("Digite o número de série do equipamento: ");
+        Console.WriteLine("Digite o número de série do equipamento: ");
         string numeroSerie = Console.ReadLine();
 
-        Console.Write("Digite o nome do fabricante do equipamento: ");
+        Console.WriteLine("Digite o nome do fabricante do equipamento: ");
         string fabricante = Console.ReadLine();
 
-        Console.Write("Digite a data de fabricação do equipamento: ");
+        Console.WriteLine("Digite a data de fabricação do equipamento: ");
         DateTime dataFabricacao = DateTime.Parse(Console.ReadLine());
 
         Equipamento equipamento = new Equipamento();
@@ -77,13 +80,52 @@ public class TelaEquipamento
         equipamento.precoAquisicao = precoAquisicao;
         equipamento.numeroSerie = numeroSerie;
         equipamento.fabricante = fabricante;
-        equipamento.dataFabricacao = dataFabricacao;    
-        
+        equipamento.dataFabricacao = dataFabricacao;
+
+        repositorioEquipamento.equipamentos[0] = equipamento;
+
         Console.WriteLine($"\nEquipamento \"{equipamento.nome}\" cadastrado com sucesso!");
+        Console.ReadLine();
+    }
+
+    public void VisualizarRegistros()
+    {
+        ExibirCabecalho();
+
+        Console.WriteLine("Visualização De Equipamentos");
+        Console.WriteLine();
+
+        Console.WriteLine(
+            "{0, -10} | {1, -20} | {2, -15} | {3, -10} | {4, -20} | {5, -15} | ",
+            "Id", "Nome", "Preço Aquisição", "Número Série", "Fabricante", "Data Fabricação"
+        );
+
+        Equipamento[] equipamentos = repositorioEquipamento.equipamentos;
+
+        for ( int i = 0; i < equipamentos.Length; i++ )
+        {
+            Equipamento e = equipamentos[i];
+
+            if (e == null)
+                continue;
+
+            Console.WriteLine(
+                "{0, -10} | {1, -20} | {2, -15} | {3, -10} | {4, -20} | {5, -15} | ",
+                e.id, e.nome, e.precoAquisicao.ToString("C2"), e.numeroSerie, e.fabricante, e.dataFabricacao.ToShortTimeString()
+            );
+        }
+
         Console.ReadLine();
     }
 }
 
+// Dados
+public class RepositorioEquipamento
+{
+    public Equipamento[] equipamentos = new Equipamento[100];
+}
+
+// Regra de negócio
 public class Equipamento
 {
     public int id;
