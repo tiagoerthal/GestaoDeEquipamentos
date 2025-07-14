c18126bbc68a4faa69cfbbd9782fdba998ef0df8
Tiago <tiagoeweber@gmail.com>
2025-07-14 16:47:54 -0300

    Implementado HomeController e ASP.NET MVC

---
+++ GestaoDeEquipamentos.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentos.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    // Ação
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ GestaoDeEquipamentos.WebApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GestaoDeEquipamentos.WebApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews(); // ASP.NET MVC - Model View Controller
+
+            WebApplication app = builder.Build();
+
+            app.UseRouting();
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
