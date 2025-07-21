7022aabb7a15620365fecdd331f6148ad141600a
Tiago <tiagoeweber@gmail.com>
2025-07-21 16:41:13 -0300

    Implementados ViewModels de Fabricante

--- GestaoDeEquipamentos.WebApp/Controllers/FabricanteController.cs
+++ GestaoDeEquipamentos.WebApp/Controllers/FabricanteController.cs
@@ -1,84 +1,107 @@
 ﻿using GestaoDeEquipamentos.Dominio.ModuloFabricante;
 using GestaoDeEquipamentos.Infraestrutura.Arquivos.Compartilhado;
 using GestaoDeEquipamentos.Infraestrutura.Arquivos.ModuloFabricante;
+using GestaoDeEquipamentos.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GestaoDeEquipamentos.WebApp.Controllers
+namespace GestaoDeEquipamentos.WebApp.Controllers;
+
+public class FabricanteController : Controller
 {
-    public class FabricanteController : Controller
+    private RepositorioFabricanteEmArquivo repositorioFabricante;
+
+    public FabricanteController()
     {
-        private RepositorioFabricanteEmArquivo repositorioFabricante;
+        ContextoDados contexto = new ContextoDados(true);
+        repositorioFabricante = new RepositorioFabricanteEmArquivo(contexto);
+    }
 
-        public FabricanteController()
-        {
-            ContextoDados contexto = new ContextoDados(true);
-            repositorioFabricante = new RepositorioFabricanteEmArquivo(contexto);
-        }
+    public IActionResult Index()
+    {
+        List<Fabricante> fabricantes = repositorioFabricante.SelecionarRegistros();
 
-        public IActionResult Index()
-        {
-            List<Fabricante> fabricantes = repositorioFabricante.SelecionarRegistros();
+        VisualizarFabricantesViewModel visualizarVm = new VisualizarFabricantesViewModel(fabricantes);
 
-            return View(fabricantes);
-        }
+        return View(visualizarVm);
+    }
 
-        public IActionResult Cadastrar()
-        {
-            return View();
-        }
+    public IActionResult Cadastrar()
+    {
+        CadastrarFabricanteViewModel cadastrarVm = new CadastrarFabricanteViewModel();
 
-        [HttpPost]
-        public IActionResult Cadastrar(string nome, string email, string telefone)
-        {
-            Fabricante novoFabricante = new Fabricante(nome, email, telefone);
+        return View(cadastrarVm);
+    }
 
-            repositorioFabricante.CadastrarRegistro(novoFabricante);
+    [HttpPost]
+    public IActionResult Cadastrar(CadastrarFabricanteViewModel cadastrarVm)
+    {
+        Fabricante novoFabricante = new Fabricante(
+            cadastrarVm.Nome,
+            cadastrarVm.Email,
+            cadastrarVm.Telefone
+        );
 
+        repositorioFabricante.CadastrarRegistro(novoFabricante);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Editar(int id)
+    {
+        Fabricante fabricanteSelecionado = repositorioFabricante.SelecionarRegistroPorId(id);
+
+        if (fabricanteSelecionado == null)
             return RedirectToAction(nameof(Index));
-        }
-        public IActionResult Editar(int id)
+
+        EditarFabricanteViewModel editarVm = new EditarFabricanteViewModel(
+            fabricanteSelecionado.Id,
+            fabricanteSelecionado.Nome,
+            fabricanteSelecionado.Email,
+            fabricanteSelecionado.Telefone
+        );
+
+        return View(editarVm);
+    }
+
+    [HttpPost]
+    public IActionResult Editar(int id, EditarFabricanteViewModel editarVm)
+    {
+        Fabricante fabricanteEditado = new Fabricante(
+            editarVm.Nome,
+            editarVm.Email,
+            editarVm.Telefone
+        );
+
+        bool edicaoConluida = repositorioFabricante.EditarRegistro(id, fabricanteEditado);
+
+        if (!edicaoConluida)
         {
-            Fabricante fabricanteSelecionado = repositorioFabricante.SelecionarRegistroPorId(id);
+            fabricanteEditado.Id = id;
 
-            if (fabricanteSelecionado == null)
-                return RedirectToAction(nameof(Index));
-
-            return View(fabricanteSelecionado);
+            return View(fabricanteEditado);
         }
 
-        [HttpPost]
-        public IActionResult Editar(int id, string nome, string email, string telefone)
-        {
-            Fabricante fabricanteEditado = new Fabricante(nome, email, telefone);
+        return RedirectToAction(nameof(Index));
+    }
 
-            bool edicaoConluida = repositorioFabricante.EditarRegistro(id, fabricanteEditado);
+    public IActionResult Excluir(int id)
+    {
+        Fabricante fabricanteSelecionado = repositorioFabricante.SelecionarRegistroPorId(id);
 
-            if (!edicaoConluida)
-            {
-                fabricanteEditado.Id = id;
-
-                return View(fabricanteEditado);
-            }
-
+        if (fabricanteSelecionado == null)
             return RedirectToAction(nameof(Index));
-        }
 
-        public IActionResult Excluir(int id)
-        {
-            Fabricante fabricanteSelecionado = repositorioFabricante.SelecionarRegistroPorId(id);
+        ExcluirFabricanteViewModel excluirVm =
+            new ExcluirFabricanteViewModel(id, fabricanteSelecionado.Nome);
 
-            if (fabricanteSelecionado == null)
-                return RedirectToAction(nameof(Index));
+        return View(excluirVm);
+    }
 
-            return View(fabricanteSelecionado);
-        }
+    [HttpPost]
+    public IActionResult ExcluirConfirmado(int id)
+    {
+        repositorioFabricante.ExcluirRegistro(id);
 
-        [HttpPost]
-        public IActionResult ExcluirConfirmado(int id)
-        {
-            repositorioFabricante.ExcluirRegistro(id);
-
-            return RedirectToAction(nameof(Index));
-        }
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ GestaoDeEquipamentos.WebApp/Models/FabricanteViewModels.cs
@@ -0,0 +1,84 @@
+﻿using GestaoDeEquipamentos.Dominio.ModuloFabricante;
+
+namespace GestaoDeEquipamentos.WebApp.Models;
+
+public class CadastrarFabricanteViewModel
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+
+    public CadastrarFabricanteViewModel()
+    {
+    }
+}
+
+public class EditarFabricanteViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+
+    public EditarFabricanteViewModel()
+    {
+    }
+
+    public EditarFabricanteViewModel(int id, string nome, string email, string telefone)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+    }
+}
+
+public class ExcluirFabricanteViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public ExcluirFabricanteViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
+public class VisualizarFabricantesViewModel
+{
+    public List<DetalhesFabricanteViewModel> Registros { get; set; }
+
+    public VisualizarFabricantesViewModel(List<Fabricante> fabricantes)
+    {
+        Registros = new List<DetalhesFabricanteViewModel>();
+
+        foreach (Fabricante f in fabricantes)
+        {
+            DetalhesFabricanteViewModel detalhesVm = new DetalhesFabricanteViewModel(
+                f.Id,
+                f.Nome,
+                f.Email,
+                f.Telefone
+            );
+
+            Registros.Add(detalhesVm);
+        }
+    }
+}
+
+public class DetalhesFabricanteViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+
+    public DetalhesFabricanteViewModel(int id, string nome, string email, string telefone)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+    }
+}
